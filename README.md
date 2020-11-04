17c146728f090126016f1e56c4960c2289c81c19
mazdaaskari <mazdaaskari@outlook.com>
2020-11-05 00:10:46 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+With the help of this app, 
+you can find music that suits your taste 
+by entering the name of your favorite singer or band.
+
---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    print('Welcome to Music Map Scraper\nThe data is received from the www.music-map.com\n')
+    artist_name = input('Please enter your favorite artist or band : ')
+    try:
+        website_address = 'https://www.music-map.com/{0}'.format(artist_name)
+        page = requests.get(website_address)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        musicMap = soup.find(id='gnodMap')
+        artists = musicMap.find_all(class_='S')
+        for artist in artists:
+            print(artist.get_text())
+    except AttributeError:
+        print('Artist not found ! \nPlease enter artist name correctly')
+    except ConnectionError:
+        print('Not connected ! \nPlease check your internet connection')
