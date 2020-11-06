9fd57735563307d79061c1df45539f5eb028b091
mazdaaskari <mazdaaskari@outlook.com>
2020-11-06 13:28:10 +0330

    add command-line commands to set artist/band name

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import click
 
 
-def __get_music_map__(artistname):
+def __get_music_map__(artistname) :
     website_address = 'https://www.music-map.com/{0}'.format(artistname)
     page = requests.get(website_address)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -12,14 +13,22 @@ def __get_music_map__(artistname):
     return artists
 
 
-if __name__ == '__main__':
+@click.command()
+@click.help_option('--help', '-h')
+@click.option('--artistname', '-a', type=str, help='Enter your favorite artist/band name', prompt='Please enter your '
+                                                                                                  'favorite '
+                                                                                                  'artist/band')
+def __get_other_artists_name__(artistname) :
+    try :
+        artists = __get_music_map__(artistname)
+        for artist in artists :
+            click.echo(artist.get_text())
+    except AttributeError :
+        click.echo('Artist not found ! \nPlease enter artist name correctly')
+    except ConnectionError :
+        click.echo('Not connected ! \nPlease check your internet connection')
+
+
+if __name__ == '__main__' :
     print('Welcome to Music Map Scraper\nThe data is received from the www.music-map.com\n')
-    artist_input = input('Please enter your favorite artist or band : ')
-    try:
-        result = __get_music_map__(artist_input)
-        for artist in result:
-            print(artist.get_text())
-    except AttributeError:
-        print('Artist not found ! \nPlease enter artist name correctly')
-    except ConnectionError:
-        print('Not connected ! \nPlease check your internet connection')
+    __get_other_artists_name__()
